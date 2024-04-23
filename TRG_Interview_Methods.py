14af77620586521e91570ef54ec18c5902d0a826
AdiMosoiu93 <130320111+AdiMosoiu93@users.noreply.github.com>
2024-04-23 23:50:25 +0300

    Add version of exercise using methods

---
+++ TRG_Interview_Methods.py
@@ -0,0 +1,43 @@
+class SequenceSumCalculator:
+    def __init__(self):
+        self.begin = 0
+        self.end = 0
+        self.step = 0
+
+    def get_input(self):
+        # Prompt the user to input the beginning value of the sequence.
+        self.begin = self.get_valid_input("Insert begin value: ")
+
+        # Prompt the user to input the end value of the sequence.
+        self.end = self.get_valid_input("Insert end value: ")
+
+        # Prompt the user to input the step value for the sequence.
+        self.step = self.get_valid_input("Insert step value: ")
+
+        # Check if begin value is greater than end, return 0
+        if self.begin > self.end:
+            print("Begin value should not be greater than end value.")
+            return 0
+
+    def get_valid_input(self, message):
+        input_value = input(message)
+        while not input_value.isdigit():
+            print("Please enter an integer number.")
+            input_value = input("Insert correct value: ")
+        return int(input_value)
+
+    def calculate_sequence_sum(self):
+        total_sum = 0
+        # Loop through the sequence and add each integer to the sum
+        for i in range(self.begin, self.end + 1, self.step):
+            total_sum += i
+        return total_sum
+
+# Create an instance of the SequenceSumCalculator class
+sequence_calculator = SequenceSumCalculator()
+
+# Get user input for sequence parameters
+sequence_calculator.get_input()
+
+# Calculate and print the total sum
+print("Total sum:", sequence_calculator.calculate_sequence_sum())
