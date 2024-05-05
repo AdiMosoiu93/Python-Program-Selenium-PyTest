5a19743f6b34f036dbfde142923818f17753a228
AdiMosoiu93 <130320111+AdiMosoiu93@users.noreply.github.com>
2024-05-05 11:18:32 +0300

    Add list() function to display the iterated numbers.

--- 2_TRG_Interview_Using_Methods.py
+++ 2_TRG_Interview_Using_Methods.py
@@ -4,6 +4,7 @@ class SequenceSumCalculator:
         self.end = 0
         self.step = 0
 
+    # Request valid parameters from the user for all necessary inputs.
     def get_input(self):
         # Prompt the user to input the beginning value of the sequence.
         self.begin = self.get_valid_input("Insert begin value: ")
@@ -14,39 +15,50 @@ class SequenceSumCalculator:
         # Prompt the user to input the step value for the sequence.
         self.step = self.get_valid_input("Insert step value: ")
 
-        # Check if begin value is greater than end, return 0
+        # Check if begin value is greater than end, and if not then return 0.
         if self.begin > self.end:
             print("Begin value should not be greater than end value.")
+            return 0
+        # Check if step value is 0, and if not then return 0.
+        elif self.step == 0:
+            print("Step value should not be 0.")
             return 0
 
     def get_valid_input(self, message):
         input_value = input(message)
+        # Make sure that the user does not input negative numbers or floating-point numbers by using the isdigit() function.
         while not input_value.isdigit():
-            print("Please enter an integer number.")
+            print("Please enter a positive integer number.")
             input_value = input("Insert correct value: ")
         return int(input_value)
 
     def calculate_sequence_sum(self):
         total_sum = 0
-        # Loop through the sequence and add each integer to the sum
+        # Loop through the sequence and add each integer to the sum.
         for i in range(self.begin, self.end + 1, self.step):
             total_sum += i
         return total_sum
 
-# Below is an alternative approach achieving the same functionality as the `range()` function used in the previous method.
-# total_sum = 0
-#         # Loop through the sequence and add each integer to the sum
-#         current_value = self.begin
-#         while current_value <= self.end:
-#             total_sum += current_value
-#             current_value += self.step
-#         return total_sum
+'''
+Below is an alternative approach achieving the same functionality as the `range()` function used in the previous method.
+total_sum = 0
+        # Loop through the sequence and add each integer to the sum
+        current_value = self.begin
+        while current_value <= self.end:
+            total_sum += current_value
+            current_value += self.step
+        return total_sum
+'''
 
-# Create an instance of the SequenceSumCalculator class
+# Create an instance of the SequenceSumCalculator class.
 sequence_calculator = SequenceSumCalculator()
 
-# Get user input for sequence parameters
-sequence_calculator.get_input()
+# Get user input for sequence parameters.
+while sequence_calculator.get_input() == 0:
+    pass  # Repeat until valid input is received.
 
-# Calculate and print the total sum
+# Calculate and print the total sum.
 print("Total sum:", sequence_calculator.calculate_sequence_sum())
+
+# Display the numbers used in the program.
+print("Numbers iterated trough:", list(range(sequence_calculator.begin, sequence_calculator.end + 1, sequence_calculator.step)))
